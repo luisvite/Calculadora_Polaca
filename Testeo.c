2816bf1d547ebaaf29c80c0b0c65832c212ad21f
Luis Enrique Vite Aquino <luis.vite0307@gmail.com>
2016-04-23 13:31:17 -0500

    ...

--- Testeo.c
+++ Testeo.c
@@ -1,1 +1,17 @@
+#include<string.h>
 #include<stdio.h>
+
+//Declaracion de prototipos y estructuras  a utilizar
+
+
+//Declaracion del main
+int main()
+{
+
+    printf("\t\tCALCULADORA POLACA\n");
+    printf("Ingrese la expresion a ser evaluada: ");
+
+    return 0;
+}
+
+//Declaracion de metodos
